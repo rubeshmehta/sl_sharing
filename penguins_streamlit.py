68125619e8cb43b2ed1c8f6fb4035299be7fd32d
rubeshmehta <rubesh.mehta@gmail.com>
2022-06-25 18:28:43 -0500

    Update penguins_streamlit.py

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -8,6 +8,13 @@ import pandas as pd
 
 import pickle
 
+from sklearn.metrics import accuracy_score
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.model_selection import train_test_split
+
+
 st.title('Penguin Classifier')
 
 st.write("This app uses 6 inputs to predict the species of penguin using "
@@ -15,6 +22,12 @@ st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below"
 
          " to get started!")
+
+password_guess = st.text_input('What is the Password?')
+
+if password_guess != st.secrets["password"]:
+
+  st.stop()
 
 penguin_df = pd.read_csv('penguins.csv')
 
